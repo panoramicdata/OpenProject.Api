6a31d84d50140066f25bdabda2e22a61ffda54c0
David Bond <david.bond@panoramicdata.com>
2025-02-14 14:54:26 +0000

    Added properties to Link

---
+++ OpenProject.Api/Data/Link.cs
@@ -0,0 +1,19 @@
+﻿using System.Text.Json.Serialization;
+
+namespace OpenProject.Api.Data;
+
+public class Link
+{
+	public required string? Href { get; set; }
+
+	public string? Title { get; set; }
+
+	[JsonPropertyName("templated")]
+	public bool IsTemplated { get; set; }
+
+	public string? Method { get; set; }
+
+	public string? Payload { get; set; }
+
+	public string? Identifier { get; set; }
+}
--- OpenProject.Api/Data/Links.cs
+++ OpenProject.Api/Data/Links.cs
@@ -1,3 +1,7 @@
 ﻿namespace OpenProject.Api.Data;
 
+/// <summary>
+/// In most cases, the API will return a dictionary of Link.
+/// However, in some cases, the API will return other objects
+/// </summary>
 public class Links : Dictionary<string, object>;
