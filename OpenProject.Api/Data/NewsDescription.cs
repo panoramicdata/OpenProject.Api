b7766a2b101aa04f93658b963ab1a68d24eb30d5
Johan Kool <94987086+JohanKool3@users.noreply.github.com>
2025-02-25 11:39:50 +0000

    Reused existing model and removed redundant duplicate

--- OpenProject.Api/Data/NewsDescription.cs
+++
@@ -1,7 +0,0 @@
-﻿namespace OpenProject.Api.Data;
-public class NewsDescription
-{
-	public string Format { get; set; } = string.Empty;
-	public string Raw { get; set; } = string.Empty;
-	public string Html { get; set; } = string.Empty;
-}
--- OpenProject.Api/Data/NewsItem.cs
+++ OpenProject.Api/Data/NewsItem.cs
@@ -4,5 +4,5 @@ public class NewsItem : IdentifiedItem<int>
 	public required string Title { get; set; }
 	public required string Summary { get; set; }
 
-	public required NewsDescription Description { get; set; }
+	public required Formattable Description { get; set; }
 }
