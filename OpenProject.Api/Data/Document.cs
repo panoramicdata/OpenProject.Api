1319070b873cd39b2923114013bcb38479225b7f
Johan Kool <94987086+JohanKool3@users.noreply.github.com>
2025-02-25 15:38:05 +0000

    Added definition for the Document object

---
+++ OpenProject.Api/Data/Document.cs
@@ -0,0 +1,21 @@
+﻿using System.Text.Json.Serialization;
+
+namespace OpenProject.Api.Data;
+/// <summary>
+/// A Document is a file containing a list of attachments
+/// </summary>
+public class Document
+{
+	[Range(1, int.MaxValue, ErrorMessage = "Id must be greater than 0")]
+	public int Id { get; set; }
+
+	[StringLength(60)]
+	public string Title { get; set; } = string.Empty;
+
+	public string? Description { get; set; }
+
+	public DateTime? CreatedAt { get; set; }
+
+	[JsonPropertyName("_links")]
+	public required Links Links { get; set; }
+}
