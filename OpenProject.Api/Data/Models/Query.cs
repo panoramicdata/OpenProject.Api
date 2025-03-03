9d628651ec1ac66ef1e0b98959cf721d284dacd3
Johan Kool <94987086+JohanKool3@users.noreply.github.com>
2025-03-03 14:36:32 +0000

    Added documentation for Query Model

--- OpenProject.Api/Data/Models/Query.cs
+++ OpenProject.Api/Data/Models/Query.cs
@@ -3,38 +3,84 @@ using System.Text.Json.Serialization;
 
 namespace OpenProject.Api.Data.Models;
 
+/// <summary>
+/// A query defines how work packages can be filtered and displayed. Clients can define a query once, store it, and use it later on to load the same set of filters and display options.
+/// <para>See <a href='https://www.openproject.org/docs/api/endpoints/queries/'/></para>
+/// </summary>
 public class Query : IdentifiedItem<int>, INamed, IHasTimestamps
 {
-	[JsonPropertyName("starred")]
-	public bool IsStarred { get; set; }
-
+	/// <summary>
+	/// Query name
+	/// </summary>
 	public string Name { get; set; } = string.Empty;
 
+	/// <summary>
+	/// A set of QueryFilters which will be applied to the work packages to determine the resulting work packages
+	/// </summary>
 	public IEnumerable<Filter> Filters { get; set; } = [];
 
-	public bool IncludeSubProjects { get; set; }
+	/// <summary>
+	/// Should sums (of supported properties) be shown?
+	/// </summary>
+	[JsonPropertyName("sums")]
+	public bool ShowSums { get; set; }
 
-	public bool Sums { get; set; }
+	/// <summary>
+	/// Should the timeline mode be shown?
+	/// </summary>
+	[JsonPropertyName("timelineVisible")]
+	public bool IsTimelineVisible { get; set; }
 
-	[JsonPropertyName("public")]
-	public bool IsPublic { get; set; }
+	/// <summary>
+	/// Which labels are shown in the timeline, empty when default
+	/// </summary>
+	public IDictionary TimelineLabels { get; set; }
 
+	/// <summary>
+	/// Which zoom level should the timeline be rendered in?
+	/// </summary>
+	public string TimelineZoomLevel { get; set; } = string.Empty;
+
+	/// <summary>
+	/// The timestamps to filter by when showing changed attributes on work packages.
+	/// </summary>
+	public IEnumerable<string> Timestamps { get; set; } = [];
+
+	/// <summary>
+	/// Which highlighting mode should the table have?	
+	/// </summary>
+	public string HighlightingMode { get; set; } = string.Empty;
+
+	/// <summary>
+	/// Should the hierarchy mode be enabled?
+	/// </summary>
+	public bool ShowHierarchies { get; set; }
+
+	/// <summary>
+	/// Should the query be hidden from the query list?
+	/// </summary>
 	[JsonPropertyName("hidden")]
 	public bool IsHidden { get; set; }
 
-	public bool TimelineVisible { get; set; }
+	/// <summary>
+	/// Can users besides the owner see the query?
+	/// </summary>
+	[JsonPropertyName("public")]
+	public bool IsPublic { get; set; }
 
-	public bool ShowHierarchies { get; set; }
+	/// <summary>
+	/// Should the query be highlighted to the user?
+	/// </summary>
+	[JsonPropertyName("starred")]
+	public bool IsStarred { get; set; }
 
-	public string TimelineZoomLevel { get; set; } = string.Empty;
-
-	public IDictionary TimelineLables { get; set; }
-
-	public IEnumerable<string> Timestamps { get; set; } = [];
-
-	public string HighlightingMode { get; set; } = string.Empty;
-
+	/// <summary>
+	/// Time of creation
+	/// </summary>
 	public DateTime CreatedAt { get; set; }
 
+	/// <summary>
+	/// Time of the most recent change to the query	
+	/// </summary>
 	public DateTime UpdatedAt { get; set; }
 }
