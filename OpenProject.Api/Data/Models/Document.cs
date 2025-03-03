41bcb42dc7e08efba7f13e590762499b4f4d4c9c
Johan Kool <94987086+JohanKool3@users.noreply.github.com>
2025-03-03 10:25:59 +0000

    Added Documentation to Document model

--- OpenProject.Api/Data/Models/Document.cs
+++ OpenProject.Api/Data/Models/Document.cs
@@ -1,13 +1,23 @@
 ﻿namespace OpenProject.Api.Data.Models;
 /// <summary>
 /// A Document is a file containing a list of attachments
+/// <para>See <a href='https://www.openproject.org/docs/api/endpoints/documents/'/></para>
 /// </summary>
 public class Document : IdentifiedItem<int>
 {
+	/// <summary>
+	/// The title of the Document
+	/// </summary>
 	[StringLength(60)]
 	public string Title { get; set; } = string.Empty;
 
+	/// <summary>
+	/// A text describing the document
+	/// </summary>
 	public string? Description { get; set; }
 
-	public DateTime? CreatedAt { get; set; }
+	/// <summary>
+	/// The time the document was created at
+	/// </summary>
+	public DateTime CreatedAt { get; set; }
 }
