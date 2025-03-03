4988cc7e707d4a622c764649a092d50e37ae5ff4
Johan Kool <94987086+JohanKool3@users.noreply.github.com>
2025-03-03 10:20:19 +0000

    Added documentation to Action model

--- OpenProject.Api/Data/Models/Action.cs
+++ OpenProject.Api/Data/Models/Action.cs
@@ -1,4 +1,14 @@
 ﻿namespace OpenProject.Api.Data.Models;
+
+/// <summary>
+/// An action describes what can be carried out within the application. Until an action becomes assigned, which turns it into a capability, it remains in the state of “could be”.
+/// <para>See <a href='https://www.openproject.org/docs/api/endpoints/actions-capabilities/'/> </para>
+/// </summary>
 public class Action : IdentifiedItem<string>
 {
+	// TODO: Name - Not Yet Implemented in Base API
+
+	// TODO: Description - Not Yet Implemented in Base API
+
+	// TODO: Modules - Not Yet Implemented in Base API
 }
