eea2656b214918ee7d59b1fd967404e6ba14a7aa
Johan Kool <94987086+JohanKool3@users.noreply.github.com>
2025-03-03 10:45:05 +0000

    Added documentation and Constraints to NewsItem Model

--- OpenProject.Api/Data/Models/NewsItem.cs
+++ OpenProject.Api/Data/Models/NewsItem.cs
@@ -1,11 +1,30 @@
 ﻿namespace OpenProject.Api.Data.Models;
+
+/// <summary>
+/// News are articles written by users in order to inform other users of important information.
+/// <para>See <a href='https://www.openproject.org/docs/api/endpoints/news/'/></para>
+/// </summary>
 public class NewsItem : IdentifiedItem<int>
 {
+	/// <summary>
+	/// The headline of the news
+	/// </summary>
+	[MaxLength(60)]
 	public required string Title { get; set; }
 
+	/// <summary>
+	/// A short summary
+	/// </summary>
+	[MaxLength(255)]
 	public required string Summary { get; set; }
 
+	/// <summary>
+	/// The main body of the news with all the details
+	/// </summary>
 	public required Formattable Description { get; set; }
 
-	public DateTime? CreatedAt { get; set; }
+	/// <summary>
+	/// The time the news was created at
+	/// </summary>
+	public DateTime CreatedAt { get; set; }
 }
