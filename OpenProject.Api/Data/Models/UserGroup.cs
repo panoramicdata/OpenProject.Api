d3fceed05f3a5e81a6907097dae4607224e9b16a
Johan Kool <94987086+JohanKool3@users.noreply.github.com>
2025-03-03 14:46:14 +0000

    Added documentation for UserGroup Model

--- OpenProject.Api/Data/Models/UserGroup.cs
+++ OpenProject.Api/Data/Models/UserGroup.cs
@@ -1,13 +1,23 @@
 ﻿namespace OpenProject.Api.Data.Models;
 
 /// <summary>
-/// User Group
+/// Groups are collections of users. They support assigning/unassigning multiple users to/from a project in one operation.
+/// <para>See <a href='https://www.openproject.org/docs/api/endpoints/groups/'/></para>
 /// </summary>
 public class UserGroup : IdentifiedItem<int>, INamed, IHasTimestamps
 {
+	/// <summary>
+	/// Group’s full name, formatting depends on instance settings	
+	/// </summary>
 	public required string Name { get; set; }
 
+	/// <summary>
+	/// Time of creation
+	/// </summary>
 	public DateTime CreatedAt { get; set; }
 
+	/// <summary>
+	/// Time of the most recent change to the user	
+	/// </summary>
 	public DateTime UpdatedAt { get; set; }
 }
